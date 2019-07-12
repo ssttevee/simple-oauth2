2784189d7b041f5637f1ba7710512191b4875552
Steve Lam <slam@akshonesports.com>
2019-07-12 10:14:17 -0700

    remove joi dependence

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 'use strict';
 
-const Joi = require('joi');
 const authCodeModule = require('./lib/client/auth-code');
 const passwordModule = require('./lib/client/password');
 const accessTokenModule = require('./lib/access-token');
 const clientCredentialsModule = require('./lib/client/client');
-
-// https://tools.ietf.org/html/draft-ietf-oauth-v2-31#appendix-A.1
-const vsCharRegEx = /^[\x20-\x7E]*$/;
-
-const optionsSchema = Joi
-  .object()
-  .keys({
-    client: Joi.object().keys({
-      id: Joi.string().regex(vsCharRegEx).allow(''),
-      secret: Joi.string().regex(vsCharRegEx).allow(''),
-      secretParamName: Joi.string().default('client_secret'),
-      idParamName: Joi.string().default('client_id'),
-    }).required(),
-    auth: Joi.object().keys({
-      tokenHost: Joi.string().required().uri({ scheme: ['http', 'https'] }),
-      tokenPath: Joi.string().default('/oauth/token'),
-      revokePath: Joi.string().default('/oauth/revoke'),
-      authorizeHost: Joi.string().default(Joi.ref('tokenHost')),
-      authorizePath: Joi.string().default('/oauth/authorize'),
-    }).required(),
-    http: Joi.object().unknown(true),
-    options: Joi.object().keys({
-      bodyFormat: Joi.any().only('form', 'json').default('form'),
-      authorizationMethod: Joi.any().only('header', 'body').default('header'),
-    }).default(),
-  });
 
 module.exports = {
 
@@ -39,9 +12,7 @@ module.exports = {
    * @param  {Object}  opts Module options as defined in schema
    * @returns {Object} The simple-oauth2 client
    */
-  create(opts = {}) {
-    const options = Joi.attempt(opts, optionsSchema, 'Invalid options provided to simple-oauth2');
-
+  create(options = {}) {
     return {
       accessToken: accessTokenModule(options),
       ownerPassword: passwordModule(options),
